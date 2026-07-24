12d67a72aee1ea2c53634aca11da2a57d9001234
Marks-L <a15235145819@163.com>
2026-07-24 13:23:18 +0800

    追加修改

--- LiteasyClaw/desktop/src/app/features/pdf/PdfReader.tsx
+++ LiteasyClaw/desktop/src/app/features/pdf/PdfReader.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState, type CSSProperties } from "react";
+import { useEffect, useMemo, useRef, useState, type CSSProperties, type MouseEvent as ReactMouseEvent } from "react";
 import {
   CommentRegular,
   DocumentRegular,
